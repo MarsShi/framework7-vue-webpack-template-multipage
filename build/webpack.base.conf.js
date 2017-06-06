3e43a812f482976a213e26bdc6ed0ee81c93e99a
s123ym <s123ym@qq.com>
2017-06-06 10:37:39 +0800

    add code

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,75 @@
+var path = require('path')
+var utils = require('./utils')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
+
+var glob = require('glob');
+var entries = getEntry('./src/**/*.js'); // 获得入口js文件
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  // entry: {
+  //   app: './src/main.js'
+  // },
+  entry: entries,
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  }
+}
+
+function getEntry(globPath) {
+  var entries = {},
+    basename, tmp, pathname;
+
+  glob.sync(globPath).forEach(function (entry) {
+    basename = path.basename(entry, path.extname(entry));
+    tmp = entry.split('/').splice(-3);
+    pathname = tmp.splice(0, 1) + '\/' + basename; // 正确输出js和html的路径
+    entries[pathname] = entry;
+  });
+  return entries;
+}
---
+++ src/module/hello/app.vue
@@ -0,0 +1,38 @@
+<template>
+    <!-- App -->
+    <div id="app">
+        <f7-views>
+            <f7-view main>
+                <f7-pages navbar-fixed toolbar-fixed>
+                    <f7-page name="home">
+                        <f7-navbar title="Hello App"></f7-navbar>
+
+                        <Hello></Hello>
+
+                        <f7-toolbar>
+                            <f7-link>Link 1</f7-link>
+                            <f7-link>Link 2</f7-link>
+                        </f7-toolbar>
+
+                        <p class="my-css">Hello,Framework7</p>
+                    </f7-page>
+                </f7-pages>
+            </f7-view>
+        </f7-views>
+    </div>
+</template>
+
+<script>
+    import Hello from '../../components/Hello.vue';
+
+    export default {
+        data () {   
+            return {
+
+            }
+        },
+        components:{
+            Hello : Hello
+        }
+    }
+</script>
